985d16758b8d1261cefe11f07e428652731ea501
renan-rod <71822588+artistadacomputacao@users.noreply.github.com>
2021-06-11 12:16:34 -0300

    Atualizar 'main.ts'

--- main.ts
+++ main.ts
@@ -1,72 +1,303 @@
-let player = {
-x: 0,
-y: 4,
-pontos: 0,
-game: game.createSprite(0, 0),
-brilho: 10,
-velocidade: 1,
-};
-let maca = {
-x: randint(0, 4),
-y: -1,
-velocidade: 0.03,
-brilho: 0,
-game: game.createSprite(0, 0),
-};
-function addmaca() {
-maca.game.delete();
-maca.game = game.createSprite(0, 0);
-maca.brilho = 20;
+// variables
+    let player = {
+        game: game.createSprite(null,null),
+        bright: 2,
+        x: 2,
+        y: 4
+
+    };
+
+    let falls_apple = 0;
+
+    let taked_apples = 0;
+
+    let gamerule = {
+        level: 1,
+        lifes: 100,
+        points: 1,
+
+    };
+
+    function gameOver(){
+        basic.showAnimation(`
+        # # # # #
+        # . . . .
+        # . # # #
+        # . . . #
+        # # # # #
+
+        `)
+
+        basic.showAnimation(`
+        # # # # #
+        # . . . #
+        # # # # #
+        # . . . #
+        # . . . #
+
+        `)
+
+        basic.showAnimation(`
+        # . . . #
+        # # . # #
+        # . # . #
+        # . . . #
+        # . . . #
+
+        `)
+
+        basic.showAnimation(`
+        # # # # #
+        # . . . .
+        # # # # #
+        # . . . .
+        # # # # #
+
+        `)
+
+        basic.showAnimation(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+
+        `)
+
+        basic.showAnimation(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+
+        `)
+
+        basic.showAnimation(`
+        # . . . #
+        # . . . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+        
+        basic.showAnimation(`
+        # # # # #
+        # . . . .
+        # # # # #
+        # . . . .
+        # # # # #
+
+        `)
+
+        basic.showAnimation(`
+        # # # . .
+        # . . # .
+        # # # . .
+        # # . . .
+        # . # . .
+
+        `)
+
+    }
+
+    let losed_apples = 0
+    let maca = {
+        game: game.createSprite(null, null),
+        x: randint(0, 4),
+        y: 0
+
+    };
+
+    function animation(){
+        game.pause()
+        basic.showNumber(gamerule.points)
+
+        basic.showAnimation(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+
+        `);
+
+        basic.showAnimation(`
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+
+        `);
+
+        basic.showAnimation(`
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+
+        `);
+
+        basic.showAnimation(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+
+        `);
+
+            basic.showAnimation(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+
+        `);
+
+        basic.showAnimation(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # .
+
+        `);
+        
+        basic.showAnimation(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # . .
+
+        `);
+
+        basic.showAnimation(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # . . .
+
+        `);
+
+        basic.showAnimation(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+
+        `);
+
+            basic.showAnimation(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+
+        `);
+
+        game.resume();
+
+    }
+
+    function addpoint(){
+        if (game.score() != gamerule.points){
+            game.setScore(gamerule.points)
+            console.log("level " + game.score())
+            animation()
+
+        }   
+
+    }
+
+    function frame(){
+
+        let velocidade = 2/ + (game.score()/10) * 2
+        maca.y += gamerule.level/(velocidade)
+
+        if (maca.y >= 5){
+            maca.game.off()
+            maca.y = randint(0, -10)
+            maca.x = randint(0, 4)
+            falls_apple++
+            console.log(falls_apple + " maçãs ja cairam"+", dessas, você pegou " + taked_apples + ", " + losed_apples+" maçãs foram perdidas")
+            maca.game.on()
+
+        }
+        if (maca.y < 0){
+            maca.game.off()
+
+        }else if (maca.y < 5){
+            maca.game.on()
+
+        }
+
+        if (player.x > 4){
+            player.x = 4;
+
+        }else if (player.x < 0){
+            player.x = 0
+
+        }
+
+        if (player.bright > 255){
+            gamerule.points++
+            player.bright = 2
+
+        }
+        losed_apples = falls_apple - taked_apples
+        if (losed_apples > 9){
+            gameOver()
+        }
+
+    }
+
+    function move(){
+        input.onButtonPressed(Button.A,()=> {
+            player.x--
+
+        })
+        input.onButtonPressed(Button.B,()=> {
+            player.x++
+
+        })
+
+    }
+
+    function collision(){
+        if (maca.x == player.x && Math.round(maca.y) == player.y){
+            maca.game.off()
+            maca.y = randint(0, -10)
+            maca.x = randint(0, 4)
+            falls_apple++
+            taked_apples++
+            console.log(falls_apple + " maçãs ja cairam"+", dessas, você pegou " + taked_apples + ", " + losed_apples+" maçãs foram perdidas")
+            maca.game.on()
+            player.bright += 50
+            
+        }
+
 }
-function setup() {
-/*setup do player*/
-player.game.setBrightness(player.brilho);
-player.game.setX(player.x);
-player.game.setY(player.y);
-game.setScore(player.pontos);
-/*setup da maçã*/
-maca.game.setBrightness(maca.brilho);
-maca.game.setX(maca.x);
-maca.game.setY(maca.y);
-}
-function inputs() {
-input.onButtonPressed(Button.A, () => {
-  player.x -= player.velocidade;
-});
-input.onButtonPressed(Button.B, () => {
-  player.x += player.velocidade;
-});
-}
-function debug() {
-if (player.x > 4) {
-  player.x = 4;
-} else if (player.x < 0) {
-  player.x = 0;
-}
-if (Math.round(maca.y) == 4 && maca.x == player.x) {
-  player.pontos++;
-  player.brilho += 20;
-  maca.y = randint(-1, -5);
-  maca.brilho = 0;
-  maca.x = randint(0, 4);
-  addmaca();
-}
-if (Math.round(maca.y) == 5) {
-  game.gameOver();
-}
-if (maca.y > 0) {
-  maca.brilho = 20;
-} else {
-  maca.brilho = 0;
-}
-}
-function frame() {
-maca.y += maca.velocidade;
-}
-basic.forever(() => {
-debug();
-setup();
-inputs();
-frame();
-});
- 
+// setups 
+    game.setScore(1);
+
+//game
+    basic.forever(()=>{
+        move()
+        player.game.setBrightness(player.bright)
+        player.game.setX(player.x)
+        player.game.setY(player.y)
+        maca.game.setX(maca.x)
+        maca.game.setY(maca.y)
+        addpoint()
+        frame()
+        collision()
+
+    })
